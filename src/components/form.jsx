75656c1c14c6dd1cb2f4f917a40240670dac89fd
Xavier Charles <ch.xavier03@gmail.com>
2022-10-25 11:53:26 +0200

    add double click on todo for modification

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -2,7 +2,7 @@ function Form({ handleAddTodo }) {
     return (
         <form className='form' onSubmit={handleAddTodo}>
             <label className="form__label" htmlFor="todo">New Todos
-                <input name="todo" type="text" placeholder='todo' />
+                <input className="form_input" name="todo" type="text" placeholder='todo' />
             </label>
             <button type="submit">Add Todo</button>
         </form>
--- src/components/todos.jsx
+++ src/components/todos.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
+
 function Todos({ todos, handleRemoveTodo }) {
+    const [isEditable, setIsEditable] = useState(false)
+
+    function handleDblClick(){
+        console.log("modifier");
+        setIsEditable(true);
+    }
+
+    function handleBlur(){
+        setIsEditable(false)
+    }
+
+
     return (
         <ul>
             {
                 todos.map((todo, index) => {
-                    return <li key={index}>{todo.description} <button className="form__btn" onClick={() => handleRemoveTodo(index)}></button></li>
+                    return <li contentEditable={isEditable} onBlur={handleBlur}  onDoubleClick={handleDblClick} key={index}>{todo.description} <button className="form__btn" onClick={() => handleRemoveTodo(index)}></button></li>
                 })
             }
         </ul>
